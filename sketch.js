3184c36a621881c01ac44df2435ae8622afdeabf
tevya123 <44813117+tevya123@users.noreply.github.com>
2018-11-20 12:20:45 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,34 @@
-var r, g, b; //declares variables for color.
-
-
+var randomColor; //lets the user know that everything will be any color.
+//var r,g,b;
 
 function setup() {
-  // put setup code here
-    createCanvas(700,700); //sets size of the canvas in browser and displays in it.
-    r = random(255); //assigns a value of a random number between 0 and 255 to r.
-    g = 0;// this assigns value of 0 to g.
-    b =random(255);
-
+  createCanvas(700, 700);
+  background(0,0,0);
 }
 
 function draw() {
-  // put drawing code here
-    background(r, g, b); //sets background color.
+    if (mouseIsPressed){ //press the mouse to see change.
+    stroke(205, 234, 21);// changes the shapes outline
+    fill(random(255), random(255), random(255));//The shape can be any color.
+    //ellipse(255,255,100,100);
+  }
+else {
+    stroke(21, 21, 234);
+    //ellipse(355,355,100,100); 
+}
+    textSize(40); //changes the size.
+    textFont("Bubble");//changes the text design.
+    //fill(239, 9, 51);
+    text("LETS GO CRAZY",200,300);//the text itself
+    //textColor(239, 9, 51);
+    
+    ellipse(mouseX+6, mouseY, mouseX+6, mouseY); //the ellipse drawing.
+    ellipse(mouseX, mouseY+6, mouseX, mouseY+6);//the ellipse drawing.
+    
 }
 
-function keyPressed(){ //when key is pressed r is assigned new random value.
-      r = random(255);
-      b = random(255);
-      background(r,g,b);
-    ellipse(mouseX,mouseY,100,100)
-    
-    if (keyCode ===  LEFT_ARROW){ //tests the valur pf key that is pressed
-        r=0; //if the left arrow has been pressed reassignes values
-        g=0;
-        b=0;
-        background(r, g,b);
-    }
-    if (keyCode === RIGHT_ARROW){
-        r=255; //if the right  arrow has been pressed reassignes values
-        g=255;
-        b=255;
-        background(r, g,b);
-    }
-    
-    
- 
-    
-    
+function keyPressed(){ //press any key to start over/clear screen.
+    background(0,0,0);//sets the background black
+    stroke(205, 234, 21);//sets the stroke color back to blue.
 }
+
